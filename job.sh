f1a48b377a7b691b9520dee252108bb305bebedc
Zakaria <zakaria.nassreddine@insa-lyon.fr>
2022-01-09 03:06:50 +0100

    Update job.sh

--- job.sh
+++ job.sh
@@ -2,5 +2,6 @@ python3 link-prediction.py deepwalk cora
 python3 link-prediction.py deepwalk blogcat
 python3 link-prediction.py node2vec epinion
 python3 link-prediction.py deepwalk epinion
-python3 link-prediction.py node2vec dblp-au
 python3 link-prediction.py node2vec twitter
+python3 link-prediction.py node2vec dblp-au
+
