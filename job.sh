bae0127a14786e89ce64da4394021b8ed96376cb
Zakaria <zakaria.nassreddine@insa-lyon.fr>
2022-01-09 02:48:25 +0100

    cluster setup

---
+++ job.sh
@@ -0,0 +1,5 @@
+python3 link-prediction.py deepwalk blogcat
+python3 link-prediction.py node2vec epinion
+python3 link-prediction.py deepwalk epinion
+python3 link-prediction.py node2vec dblp-au
+python3 link-prediction.py node2vec twitter
