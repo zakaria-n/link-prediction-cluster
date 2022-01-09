ef89c561e110868dd3c74411d140655fc63b7f33
Zakaria <zakaria.nassreddine@insa-lyon.fr>
2022-01-09 12:02:52 +0100

    LP scores n2v dw

--- job.sh
+++ job.sh
@@ -1,5 +1,3 @@
-python3 link-prediction.py deepwalk cora
-python3 link-prediction.py deepwalk blogcat
 python3 link-prediction.py node2vec epinion
 python3 link-prediction.py deepwalk epinion
 python3 link-prediction.py node2vec twitter
